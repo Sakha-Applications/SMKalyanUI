f1a92a740c03c97054df212eec676a450652190e
SatyabodhVarkur <varkur@gmail.com>
2025-05-24 20:43:19 +0530

    correct back home button

--- src/hooks/BackHomeButton.js
+++ src/hooks/BackHomeButton.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function BackHomeButton() {
   return (
-    <Link to="/" className="inline-block bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-md mt-4">
+    <Link to="/home" className="inline-block bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-md mt-4">
       Back to Home
     </Link>
   );
