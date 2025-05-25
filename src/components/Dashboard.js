a82464c1d46245eae8b6e01ca3db82913f556e97
SatyabodhVarkur <varkur@gmail.com>
2025-05-25 18:27:10 +0530

    corrected the path

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import PreferredProfilesSection from "./components/PreferredProfiles/PreferredProfilesSection";
+import PreferredProfilesSection from './prefferedProfile/PreferredProfilesSection';
 
 const Dashboard = () => {
   const navigate = useNavigate();
